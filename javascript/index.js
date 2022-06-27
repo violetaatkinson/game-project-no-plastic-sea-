46c56adcb5599e7054db11ae06f68e55c4770aeb
violeta atkinson <violeta.atkinson@gmail.com>
2022-06-27 19:28:35 +0200

    play-hidden-game-board

--- javascript/index.js
+++ javascript/index.js
@@ -5,10 +5,15 @@ const startBtn = document.getElementById('start-btn')
 const lifes = document.querySelectorAll('.life')
 const containerHearts = document.getElementById('hearts')
 const points = document.getElementById('points')
+const title = document.querySelector('h1')
+const buttons = document.getElementById('buttons')
 
 startBtn.addEventListener('click', (life, point) => {
   if (game.intervalId === null) {
+    const gameBoardNode = document.getElementById('game-board');
+    gameBoardNode.classList.add('visibility');
     game.start()
+    title.classList.remove('visibility')
     for (life of lifes) {
       life.classList.remove('visibility')
     }
